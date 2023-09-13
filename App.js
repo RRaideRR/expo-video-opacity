48286dc39d6689246b56d5a6b02882cd5db01687
philipp <philipp.reiter90@gmail.com>
2023-09-13 15:14:45 +0200

    first commit

--- App.js
+++ App.js
@@ -1,11 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { ResizeMode, Video } from 'expo-av';
 
 export default function App() {
+  const [opacity, setOpacity] = useState(1);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={{flex: 1, width: '100%'}}>
+        <Video
+          source={require('./assets/lake.mp4')}
+          style={{width: '100%', height: '100%', opacity: opacity}}
+          shouldPlay={true}
+          isLooping
+          resizeMode={ResizeMode.COVER}
+        />
+      </View>
+      <Button title={"Click me to toggle opacity"} onPress={() => setOpacity(opacity === 1 ? 0.50 : 1)}/>
     </View>
   );
 }
@@ -16,5 +28,6 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    marginBottom: 30
   },
 });
